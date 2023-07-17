6e87e2487fa5d6542eaa51222ea2628b4593b5a5
Kağan Uğur <kgnugur@gmail.com>
2023-07-17 19:02:59 +0300

    feat: Add theme button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils"
 import "./globals.css"
 import { PokeBall } from "@/components/poke-ball"
+import { ThemeToggle } from "@/components/theme-toggle"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import Link from "next/link"
@@ -29,7 +30,7 @@ export default function RootLayout({
       >
         <Providers>
           <header className="sticky top-0 z-40 w-full border-b bg-background">
-            <div className="container flex h-16 items-center justify-between">
+            <div className="container flex h-16 items-center justify-between gap-4">
               <Link href="/" className="select-none font-bold">
                 Pokedex
               </Link>
@@ -38,6 +39,24 @@ export default function RootLayout({
           </header>
 
           <main>{children}</main>
+
+          <footer className="py-6 md:px-8 md:py-0">
+            <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
+              <div>
+                Built by Kagan Ugur. Source code is available on{" "}
+                <Link
+                  href="https://github.com/thekaganugur/next-pokedex"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  GitHub
+                </Link>
+                <span>.</span>
+              </div>
+
+              <ThemeToggle />
+            </div>
+          </footer>
         </Providers>
       </body>
     </html>
